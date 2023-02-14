d902a95b3bbb33d972edb26346c116d09a6004c8
CJ Madushka <79196915+cjmadushka@users.noreply.github.com>
2023-02-14 11:29:52 +0530

    First Prototype

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import cv2
 from Recognizer import Recognizer
 
-# Encode faces from a folder
-sfr = Recognizer()
-sfr.load_encoding_images("images/")
 
-# Load Camera
+recognizer = Recognizer()
+recognizer.load_encoding_images("images/")
+
+
 cap = cv2.VideoCapture(0)
 
 
 while True:
     ret, frame = cap.read()
 
-    # Detect Faces
-    face_locations, face_names = sfr.detect_known_faces(frame)
+    face_locations, face_names = recognizer.detect_known_faces(frame)
     for face_loc, name in zip(face_locations, face_names):
         y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
 
